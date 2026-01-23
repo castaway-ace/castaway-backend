29dd23e2a2a050f9948898dab27d56098fa43800
Anthony Ostia <meximanianace@gmail.com>
2026-01-22 23:32:19 -0800

    fix: reintializing again

---
+++ backend/prisma/migrations/20260121011842_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `token_type` on the `accounts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "accounts" DROP COLUMN "token_type";
---
+++ backend/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  const port = process.env.BACKEND_PORT || 3000;
+  await app.listen(port);
+
+  console.log(`Castaway running on http://localhost:${port}`);
+}
+
+bootstrap();
