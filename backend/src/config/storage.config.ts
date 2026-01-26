4acfdf6e186b46ef9f79053f858afe3ec8e041fb
Anthony Ostia <meximanianace@gmail.com>
2026-01-26 13:21:50 -0800

    fix: password and user

--- backend/src/config/storage.config.ts
+++ backend/src/config/storage.config.ts
@@ -3,8 +3,8 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('storage', () => ({
   endpoint: process.env.MINIO_ENDPOINT || 'minio',
   port: parseInt(process.env.MINIO_PORT || '9000', 10),
-  accessKey: process.env.MINIO_ACCESS_KEY || '',
-  secretKey: process.env.MINIO_SECRET_KEY || '',
+  accessKey: process.env.MINIO_ROOT_USER || '',
+  secretKey: process.env.MINIO_ROOT_PASSWORD || '',
   useSSL: process.env.MINIO_USE_SSL === 'true',
   bucketName: process.env.MINIO_BUCKET_NAME || 'castaway-audio',
   region: process.env.MINIO_REGION || 'us-west-2',
