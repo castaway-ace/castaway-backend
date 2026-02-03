2794fe4478b3c34819c5085ea8811b0819f25fcc
Anthony Ostia <meximanianace@gmail.com>
2026-02-02 16:25:48 -0800

    Update optional-oauth.guard.ts

--- src/auth/guards/optional-oauth.guard.ts
+++ src/auth/guards/optional-oauth.guard.ts
@@ -1,20 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../../generated/prisma/client.js';
 
 @Injectable()
 export class OptionalAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const result = await super.canActivate(context);
+      return Boolean(result);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (_error: unknown) {
+      return true;
+    }
+  }
+
   handleRequest<TUser = User>(
     err: Error | null,
     user: TUser,
   ): TUser | undefined {
-    // No error thrown if user is not found
-    // Just returns undefined
-
-    if (err || !user) {
-      return undefined;
-    }
-
-    return user;
+    return user || undefined;
   }
 }
