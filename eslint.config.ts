ad5c65ad02839b4cb2a1393fe79c1973c0493f50
Anthony Ostia <meximanianace@gmail.com>
2026-04-21 10:32:49 -0700

    set up eslint

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default defineConfig([
+  {
+    ignores: [
+      'eslint.config.ts',
+      'src/generated/**',
+      'dist/**',
+    ],
+  },
+  {
+    files: ['**/*.{js,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  tseslint.configs.strictTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
+  },
+]);
