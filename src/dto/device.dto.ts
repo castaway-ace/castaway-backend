9afea4df29b85bb375321a2dd9784048fc3bfe94
Anthony Ostia <meximanianace@gmail.com>
2026-05-23 13:57:05 -0700

    fix to uuid device client id

--- src/dto/device.dto.ts
+++ src/dto/device.dto.ts
@@ -1,8 +1,8 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class DeviceInfoDto {
   @IsNotEmpty()
-  @IsString()
+  @IsUUID()
   readonly clientId!: string;
 
   @IsOptional()
