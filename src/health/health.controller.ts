8f06d0d4797fc7f9e044b78ad2b2197d2238f7b5
Anthony Ostia <meximanianace@gmail.com>
2026-05-27 17:55:47 -0700

    add local ports and make health public

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import {
   HealthCheckService,
   HealthCheck,
@@ -6,11 +6,8 @@ import {
 } from '@nestjs/terminus';
 import { StorageHealthIndicator } from './storage.health.js';
 import { PrismaService } from '../prisma/prisma.service.js';
-import { AuthGuard } from '../auth/guards/auth.guard.js';
-import { AdminGuard } from '../auth/guards/admin.guard.js';
 
 @Controller('health')
-@UseGuards(AuthGuard, AdminGuard)
 export class HealthController {
   constructor(
     private health: HealthCheckService,
