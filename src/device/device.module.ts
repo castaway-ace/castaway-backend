85eafadeb93975abf3d3f3a66e8fd9ca3170ad49
Anthony Ostia <meximanianace@gmail.com>
2026-06-18 16:52:54 -0700

    update device module

--- src/device/device.module.ts
+++ src/device/device.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service.js';
 import { DeviceService } from './device.service.js';
 
 @Module({
-  providers: [DeviceService, PrismaService],
+  providers: [DeviceService],
   exports: [DeviceService],
 })
 export class DeviceModule {}
